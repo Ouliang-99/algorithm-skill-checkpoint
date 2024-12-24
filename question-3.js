6cdf06c9ecd3dc56ec87002c22016964be005309
OLEANG <wannachok.job@gmail.com>
2024-12-24 10:36:27 +0700

    oleang answer for algorithm skill csheckpoint

--- question-3.js
+++ question-3.js
@@ -2,6 +2,7 @@
 What is the Big O notation for Function findStudentById . Explain your reason.
 Answer:
 
+O(n) because it's need to loop through all value in array for complaration id and studentId.
 
 */
 
@@ -9,12 +10,14 @@ Answer:
 What is the Big O notation for Function findProductPrice. Explain your reason.
 Answer:
 
-
+o log(n) because it's don't need to loop through all value in array ,it's just check in middle of array , cut it haft if the value not a search value and it's will cut it haft again untill find match value.
+ 
 */
 
 /* 
 Which function is more efficient and why?
 Answer:
 
+o log(n) because it's don't need to loop through all value in array , that's why o log(n) is efficient and faster than o(n) 
 
 */
--- question-5.js
+++ question-5.js
@@ -2,5 +2,7 @@
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
 
+o(n^2) because it's have an loop in side loop and bubble sort will loop through all value in array for complaration,
+example : when 1st round loop completed to find 1st value and next for find a 2nd value will loop again untill sort all of value in array , it's will end up.
 
 */
